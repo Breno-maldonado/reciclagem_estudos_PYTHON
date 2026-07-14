745b7fa539a0304ed2cf35ab1a0917b4c366dafd
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-14 16:39:23 -0300

    aula 3: INT / FLOAT / STR / BOOL

---
+++ aula_3.py
@@ -0,0 +1,19 @@
+int # armazena numeros inteiros: 1, 2, 3...
+
+float # armazena numeros com ponto flutuante: 1.1, 2.40, 3.04...
+
+str # armazena strings, texto ou caracteres: 'olá', 'tchau', "123"
+
+bool # armazena valores booleanos, verdadeiro ou falso: True, False
+
+idade = 2
+print('a idade é',idade,'e do tipo',type(idade))
+
+dinheiro = 20.00
+print('o dinheiro é',dinheiro,'e do tipo',type(dinheiro))
+
+nome = 'Breno'
+print('o nome é',nome,'e do tipo',type(nome))
+
+condicao = True
+print('a condicao é',condicao,'e do tipo',type(condicao))
