0ae2194364c8f0a10b276fc0ca9f758d30ed821c
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-13 21:57:09 -0300

    aula 2: variavel

---
+++ aula_2.py
@@ -0,0 +1,9 @@
+variavel = 1 # a variavel e capaz a armazenar na memoria o conteudo que for passado a ela
+
+idade = 23 # aqui criamos a variavel chamada idade onde ela vai guardar a minha idade atual
+print(idade) # passando essa variavel dentro do print podemos imprimir o valor dela
+
+nome = 'Breno'
+print(nome)
+
+print(nome, idade)
