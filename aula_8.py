3647e1ff908450da308b7aaec46940d4162e5667
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-22 21:20:02 -0300

    aula 8: WHILE

---
+++ aula_8.py
@@ -0,0 +1,8 @@
+# existe tambem estruturas de repetições, conhecida como WHILE e FOR
+# o WHILE vai repetir uma condição enquanto ela for verdadeira
+
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador +=1
