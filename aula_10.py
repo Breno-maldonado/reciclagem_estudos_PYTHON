cbbaae520826ecc4f63625d1ddcdf48e516a6a65
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-23 23:39:30 -0300

    aula 10: listas

---
+++ aula_10.py
@@ -0,0 +1,13 @@
+# conseguimos criar listas, atraves de colchetes "[]"
+# ex: lista = ['Uma lista']
+
+
+# ordem
+#var     [-0]    [-1] [-2]  [-3] ou
+#var     [0]     [1]  [2]   [3]...
+lista = ['Breno', 23, 2002, True]
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
