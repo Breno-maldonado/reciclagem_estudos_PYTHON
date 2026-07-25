304a1931f4e736c5df888aa0fed85d6fe95a8c02
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-24 23:30:20 -0300

    aula 11: LEN() / :

---
+++ aula_11.py
@@ -0,0 +1,11 @@
+# manipulação de listas
+#len() tras a quantidade de elementos dentro de uma lista
+
+lista = ["Breno",23,2002]
+print(len(lista))
+
+# partição ":"
+print(lista[1:2])
+print(lista[:2])
+print(lista[2:])
+print(lista[:])
