a7514bd21ae34d4bc1df1c39720584758ea67a93
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-01 22:07:21 -0300

    aula: from e import

---
+++ orientacaoObjeto/oo-sabor-express/app.py
@@ -0,0 +1,13 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_mexicano = Restaurante('mexican food', 'mexicana')
+restaurante_japones = Restaurante('sushi', 'japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
