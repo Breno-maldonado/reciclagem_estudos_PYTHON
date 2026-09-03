ab904ba75f1a442c1b63b10078b2b5453826b664
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-03 12:53:30 -0300

    aula: lista de avaliacoes

--- orientacaoObjeto/oo-sabor-express/modelos/restaurante.py
+++ orientacaoObjeto/oo-sabor-express/modelos/restaurante.py
@@ -11,17 +11,17 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self._nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
-        return '⌧' if self._ativo else '☐'
+        return '☒' if self._ativo else '☐'
 
     def alternar_estado(self):
         self._ativo = not self._ativo
@@ -34,7 +34,7 @@ class Restaurante:
     def media_avaliacoes(self):
         if not self._avaliacao:
             return 0
-        soma_das_notas =sum(avaliacao._notas for avaliacao in self._avaliacao)
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
