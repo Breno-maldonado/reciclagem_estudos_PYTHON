04193496be82e27ad6e2b45ced4c27776fc704b7
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-04 11:40:45 -0300

    aula: avaliacao

--- orientacaoObjeto/oo-sabor-express/app.py
+++ orientacaoObjeto/oo-sabor-express/app.py
@@ -2,8 +2,8 @@ from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'gourmet')
 restaurante_praca.receber_avaliacao('Breno', 10)
-restaurante_praca.receber_avaliacao('Ana', 9)
-restaurante_praca.receber_avaliacao('Mila', 6)
+restaurante_praca.receber_avaliacao('Ana', 8)
+restaurante_praca.receber_avaliacao('Mila', 2)
 
 def main():
     Restaurante.listar_restaurantes()
